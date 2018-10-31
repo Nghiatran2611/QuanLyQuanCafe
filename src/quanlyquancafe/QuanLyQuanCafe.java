25fe0b74ec3fe2189fc0f7e20b5d9baca0caf5e6
mac <mac@macs-MacBook-Pro.local>
2018-10-31 16:12:56 +0700

    Thông tin bàn của quán

--- src/quanlyquancafe/QuanLyQuanCafe.java
+++ src/quanlyquancafe/QuanLyQuanCafe.java
@@ -19,11 +19,12 @@ public class QuanLyQuanCafe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
